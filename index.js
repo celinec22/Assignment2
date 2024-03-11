f3d88724fafadc867c78dea3fafe2603be290e35
Celine Chahine <122329401+celinec22@users.noreply.github.com>
2024-03-10 23:05:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 app.use('/api', apiRouter);
 
 
-// Start the server and listen on the specified port
+// Start the server and listen on port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
